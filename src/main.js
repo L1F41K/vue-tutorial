a989006af89b21ce21c725b8e1ebc59d63fc857e
L1F41K <131748880+L1F41K@users.noreply.github.com>
2024-12-16 00:37:11 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import './assets/main.css'
+import '/assets/main.css'
 
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
-import App from './App.vue'
-import Favorites from './pages/Favorites.vue'
-import Home from './pages/Home.vue'
+import App from '/App.vue'
+import Favorites from '/pages/Favorites.vue'
+import Home from '/pages/Home.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
