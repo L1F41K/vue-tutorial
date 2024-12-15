46806a4d6277ce7b8a45289439faa7b8bf15b2cb
L1F41K <lifakcanal@gmail.com>
2024-12-16 00:36:47 +0700

    Updates

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,24 @@
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+import CardList from '../components/CardList.vue'
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://4d351d1f3a5c23a2.mokky.dev/favorites')
+
+    favorites.value = data.map((favorite) => favorite.item)
+  } catch (err) {
+    console.log(err)
+  }
+})
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold">Мои закладки</h1>
+  <div class="mt-10">
+    <CardList :items="favorites" is-favorites />
+  </div>
+</template>
